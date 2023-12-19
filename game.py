db8a49ffdd9cafa047e86cc5db616f4944251d51
unknown <c0b22047ae@edu.teu.ac.jp>
2023-12-19 17:53:05 +0900

    キーを押して障害物無視

--- game.py
+++ game.py
@@ -4,30 +4,34 @@ import sys
 
 pg.init()
 
-WIDTH, HEIGHT = 400, 600
-screen = pg.display.set_mode((WIDTH, HEIGHT))
+WIDTH, HEIGHT = 400, 600  #ゲームウィンドウの幅
+screen = pg.display.set_mode((WIDTH, HEIGHT))  #ゲームウィンドウの高さ
 pg.display.set_caption("車ゲーム")
 
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
+WHITE = (255, 255, 255) #白
+RED = (255, 0, 0)  #赤
 
 player_width, player_height = 50, 50
 player_x = WIDTH // 2 - player_width // 2
 player_y = HEIGHT - player_height -20
-player_speed = 5
+player_speed = 10
 
-obstacle_width, obstacle_height = 50, 50
+obstacle_width, obstacle_height = 40, 40
 obstacle_speed = 5
 
 def draw_player(x, y):
-    pg.draw.rect(screen, WHITE, [x, y, player_width, player_height])
+    pg.draw.rect(screen, WHITE, [x, y, player_width, player_height])  #プレイヤーの車
 def draw_obstacle(x, y):
-    pg.draw.rect(screen, RED, [x, y, obstacle_width, obstacle_height])
+    pg.draw.rect(screen, RED, [x, y, obstacle_width, obstacle_height])  #障害物
 def game():
     clock = pg.time.Clock()
 
     obstacles = []
-    score = 0
+    score = 0  #スコア
+    lives = 3  #プレイヤーのライフを設定
+    gasoline = 100  #ガソリン
+    global player_x
+    font = pg.font.Font(None,36)  # フォントオブジェクトを作成
 
     while True:
         for event in pg.event.get():
@@ -39,24 +43,40 @@ def game():
 
         player_x = max(0, min(WIDTH - player_width, player_x))
 
-        if random.randint(1, 10) == 1:
+        if keys[pg.K_RETURN]:
+            gasoline -= 1
+            if gasoline == 0:  #ガソリンが0になったら
+                    print(f"Game Over! Score: {score}")
+                    pg.quit()
+                    sys.exit()
+
+        if random.randint(1, 20) == 1:
             obstacle_x = random.randint(0, WIDTH - obstacle_width)
             obstacle_y = -obstacle_height
             obstacles.append([obstacle_x, obstacle_y])
 
         for obstacle in obstacles:
-            obstacle[1] +- obstacle_speed
+            obstacle[1] += obstacle_speed
             if(
                 player_x < obstacle[0] + obstacle_width
                 and player_x + player_width > obstacle[0]
                 and player_y < obstacle[1] + obstacle_height
                 and player_y + player_height > obstacle[1]
             ):
-                print(f"Game Over! Score: {score}")
-                pg.quit()
-                sys.exit()
+                if not keys[pg.K_RETURN]:
+                    lives -= 1   #障害物に当たったらライフ -1
+                    score -= 1  #障害物に当たったらスコア　-1
+                if lives == 0 or score < 0:  #ライフが0になったらorスコアが0より小さくなったら
+                    print(f"Game Over! Score: {score}")
+                    pg.quit()
+                    sys.exit()
 
         screen.fill((0, 0, 0))
+        
+        gasoline_text = font.render(f"gasoline{gasoline}",True,(255,255,255))
+        lives_text = font.render(f"Lives: {lives}", True, (255, 255, 255))  # ライフの数を描画するテキストを作成
+        screen.blit(gasoline_text, (30, 30))  # テキストを画面上に描画
+        screen.blit(lives_text, (10, 10))  # テキストを画面上に描画
 
         draw_player(player_x, player_y)
 
@@ -66,7 +86,6 @@ def game():
         score += 1
 
         pg.display.flip()
-
         clock.tick(30)
 
 if __name__ == "__main__":
